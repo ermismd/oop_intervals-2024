7185e061753c58ddbf0039f172b6cead84756df0
ermismd <ermismdelopoulos@outlook.com>
2024-05-31 20:30:02 +0200

    formal documentation of the class interval

--- interval_oop/src/interval_oop/interval.java
+++ interval_oop/src/interval_oop/interval.java
@@ -1,5 +1,13 @@
 package interval_oop;
 
+
+
+/**
+ * 
+ * @invar|this.getLowerBound()<=this.getUpperBound()
+ * @invar|this.getLength()==this.getUpperBound()-this.getLowerBound()
+ * 
+ */
 public class interval {
 
 	private int lowerbound;
@@ -13,16 +21,46 @@ public class interval {
 	public int getUpperBound() {
 		return this.length+this.lowerbound;
 	}
-	
-	public interval(int initialLowerbound,int initialUpperBound) {
-		this.lowerbound=initialLowerbound;
-		this.length=initialUpperBound-initialLowerbound;
+	public int getLength() {
+		return this.length;
 	}
 	
+	/**
+	 * 
+	 * @pre|initialLowerBound<=initialUpperBound
+	 * @post|getLowerBound()==initialLowerBound
+	 * @post|getUpperBound()==initialUpperBound
+	 * 
+	 */
+	public interval(int initialLowerBound,int initialUpperBound) {
+		this.lowerbound=initialLowerBound;
+		this.length=initialUpperBound-initialLowerBound;
+	}
+	
+	/**
+	 * @pre|newLowerBound<=getUpperBound()
+	 * 
+	 * @mutates|this
+	 * 
+	 * @post|getLowerBound()==newLowerBound
+	 * @post|getLength()==old(getLength())
+	 * 
+	 */
 	public  void setLowerBound(int newLowerBound) {
 		this.lowerbound=newLowerBound;
 	}
 	
+	/**
+	 * 
+	 * @pre|newUpperBound>=getLowerBound()
+	 * 
+	 * @mutates|this
+	 * 
+	 * @post|getLength()== newUpperBound-getLowerBound()
+	 * @post|getUpperBound()==newUpperBound
+	 * 
+	 * 
+	 */
 	public  void setUpperBound( int newUpperBound) {
 		this.length=newUpperBound-this.lowerbound;
 	}
